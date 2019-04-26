1fe41e3ec40d63cea4bdc79a502da3d28937b19a
Steve <Steve@Steve.local>
2019-04-26 13:26:21 -0700

    fixed update ratings

--- includes/edit.inc.php
+++ includes/edit.inc.php
@@ -30,10 +30,10 @@
                 //Create array
 
                 while ($row = mysqli_fetch_assoc($result)) {
-                    echo '<div class="form-group col-md-3"><input class="form-control form-control-sm" type="text" name="idRatings" value="'.$row['idRatings'].'"></div>';
-                    echo '<div class="form-group col-md-3"><input class="form-control form-control-sm" type="text" name="listNameRatings" value="'.$row['listNameRatings'].'"></div>';
-                    echo '<div class="form-group col-md-3"><input class="form-control form-control-sm" type="text" name="itemRatings" value="'.$row['itemRatings'].'"></div>';
-                    echo '<div class="form-group col-md-2"><input class="form-control form-control-sm" type="number" name="ratingRatings" value="'.$row['ratingRatings'].'"></div>';
+                    echo '<input type="hidden" name="idRatings[]" value="'.$row['idRatings'].'">';
+                    echo '<div class="form-group col-md-5"><input class="form-control form-control-sm" type="text" name="listNameRatings[]" value="'.$row['listNameRatings'].'"></div>';
+                    echo '<div class="form-group col-md-5"><input class="form-control form-control-sm" type="text" name="itemRatings[]" value="'.$row['itemRatings'].'"></div>';
+                    echo '<div class="form-group col-md-2"><input class="form-control form-control-sm" type="number" name="ratingRatings[]" value="'.$row['ratingRatings'].'"></div>';
                 }
             }
         }
@@ -49,30 +49,18 @@
 
         if (isset($_POST['btn-update'])) {
 
+            $counter = count($_POST['idRatings']);
             $idRatings = $_POST['idRatings'];
             $listNameRatings = $_POST['listNameRatings'];
             $itemRatings = $_POST['itemRatings'];
             $ratingRatings = $_POST['ratingRatings'];
-            
-
-            $update = "UPDATE ratings SET listNameRatings=?, itemRatings=?, ratingRatings=? WHERE idRatings=?";
-            $stmt = mysqli_stmt_init($conn);
-
-            if (!mysqli_stmt_prepare($stmt, $update)) {
-                header("Location: ../yourlists.php?error=sqlerror");
-                exit();
-            }
-            else
-            {
-                //Loop through array to update each row
-
-                foreach ($row as $id) {
-                    mysqli_stmt_bind_param($stmt, "sssi", $listNameRatings, $itemRatings, $ratingRatings, $idRatings);
-                    mysqli_stmt_execute($stmt, [$idRatings => $id]);
-                }
-                header("Location: ../yourlists.php?edit=success");
-                exit();
+            for($i=0; $i<$counter; $i++){
+                $stmt = $conn->prepare("UPDATE ratings SET listNameRatings=?, itemRatings=?, ratingRatings=? WHERE idRatings=?");
+                $stmt->bind_param('ssii', $listNameRatings[$i], $itemRatings[$i], $ratingRatings[$i], $idRatings[$i]);
+                $stmt->execute();
             }
             mysqli_stmt_close($stmt);
             mysqli_close($conn);
+            header("Location: yourlists.php?edit=success");
+            exit;
         }
